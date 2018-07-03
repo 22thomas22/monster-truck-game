52e1c7f40bc02c683265fbaca285e54e451505c3
smj-edison <stephen.mason.jones@gmail.com>
2018-07-03 12:31:35 -0600

    Fixed resizer

--- src/resizer.js
+++ src/resizer.js
@@ -1,8 +1,10 @@
-document.onresize = function() {
+window.addEventListener("resize", function() {
+	console.log("resize");
+
 	width = document.documentElement.clientWidth;
 	height = document.documentElement.clientHeight;
 	
 	cnv.width = width;
 	cnv.height = height;
-};
+});
 
